a0e6b6ec13867f343952b9365fa43ef7ee316d99
Hicran Gün <hhicrangun@gmail.com>
2025-06-03 00:10:05 +0300

    Sql2

--- Hicran/SQLQuery8.sql
+++ Hicran/SQLQuery8.sql
@@ -1,24 +1,20 @@
-USE SiparisYonetim; -- Doðru veritabanýnda çalýþtýðýmýzdan emin olalým
+USE SiparisYonetim;
 GO
 
--- Mevcut tüm masa kayýtlarýný sil (önceki adýmda yaptýysan tekrar yapmana gerek yok)
 DELETE FROM TableInfo; 
--- IDENTITY_INSERT'i aç (Eðer table_id'ye manuel deðer atayacaksak, ancak IDENTITY kullandýðýmýz için buna gerek yok)
--- SET IDENTITY_INSERT TableInfo ON;
 
 -- 10 adet yeni masa ekle
-INSERT INTO TableInfo (status) VALUES ('Boþ'); -- Masa 1
-INSERT INTO TableInfo (status) VALUES ('Boþ'); -- Masa 2
-INSERT INTO TableInfo (status) VALUES ('Boþ'); -- Masa 3
-INSERT INTO TableInfo (status) VALUES ('Boþ'); -- Masa 4
-INSERT INTO TableInfo (status) VALUES ('Boþ'); -- Masa 5
-INSERT INTO TableInfo (status) VALUES ('Boþ'); -- Masa 6
-INSERT INTO TableInfo (status) VALUES ('Boþ'); -- Masa 7
-INSERT INTO TableInfo (status) VALUES ('Boþ'); -- Masa 8
-INSERT INTO TableInfo (status) VALUES ('Boþ'); -- Masa 9
-INSERT INTO TableInfo (status) VALUES ('Boþ'); -- Masa 10
+INSERT INTO TableInfo (status) VALUES ('BoÃ¾'); -- Masa 1
+INSERT INTO TableInfo (status) VALUES ('BoÃ¾'); -- Masa 2
+INSERT INTO TableInfo (status) VALUES ('BoÃ¾'); -- Masa 3
+INSERT INTO TableInfo (status) VALUES ('BoÃ¾'); -- Masa 4
+INSERT INTO TableInfo (status) VALUES ('BoÃ¾'); -- Masa 5
+INSERT INTO TableInfo (status) VALUES ('BoÃ¾'); -- Masa 6
+INSERT INTO TableInfo (status) VALUES ('BoÃ¾'); -- Masa 7
+INSERT INTO TableInfo (status) VALUES ('BoÃ¾'); -- Masa 8
+INSERT INTO TableInfo (status) VALUES ('BoÃ¾'); -- Masa 9
+INSERT INTO TableInfo (status) VALUES ('BoÃ¾'); -- Masa 10
 
--- SET IDENTITY_INSERT TableInfo OFF;
+
 GO
 
--- Masalarý kontrol etmek için:
